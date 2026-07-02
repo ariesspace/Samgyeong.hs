79093c74060a6b8b3a774a0e4035c23f3db4a85f
ariesspace <ariessie.yj@gmail.com>
2026-07-02 14:59:26 +0900

    Move post actions below content

--- app/views/post-detail.php
+++ app/views/post-detail.php
@@ -2,15 +2,6 @@
     <header class="post-detail-head">
         <a class="back-link" href="/board/<?= e($board['slug']) ?>">목록으로</a>
         <h1><?= e($post['title']) ?></h1>
-        <?php if ($canManage): ?>
-            <div class="post-manage-actions">
-                <a class="button ghost-button" href="/board/<?= e($board['slug']) ?>/post/<?= e((string) $post['id']) ?>/edit">수정</a>
-                <form method="post" action="/board/<?= e($board['slug']) ?>/post/<?= e((string) $post['id']) ?>/delete" onsubmit="return confirm('이 게시글을 삭제할까요?');">
-                    <input type="hidden" name="csrf" value="<?= e(csrf_token()) ?>">
-                    <button class="danger-button" type="submit">삭제</button>
-                </form>
-            </div>
-        <?php endif; ?>
         <dl class="post-meta">
             <div>
                 <dt>분류</dt>
@@ -44,5 +35,14 @@
 
     <footer class="post-detail-foot">
         <a class="button secondary-button" href="/board/<?= e($board['slug']) ?>">목록</a>
+        <?php if ($canManage): ?>
+            <div class="post-manage-actions">
+                <a class="button ghost-button" href="/board/<?= e($board['slug']) ?>/post/<?= e((string) $post['id']) ?>/edit">수정</a>
+                <form method="post" action="/board/<?= e($board['slug']) ?>/post/<?= e((string) $post['id']) ?>/delete" onsubmit="return confirm('이 게시글을 삭제할까요?');">
+                    <input type="hidden" name="csrf" value="<?= e(csrf_token()) ?>">
+                    <button class="danger-button" type="submit">삭제</button>
+                </form>
+            </div>
+        <?php endif; ?>
     </footer>
 </article>
